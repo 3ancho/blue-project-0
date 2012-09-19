365d8e8c5d3658aa5080deaafecdf029ee140fc7
Ruoran Wang <ruoranwang@gmail.com>
2012-09-19 15:01:17 -0400

    options

--- hrb-project0.py
+++ hrb-project0.py
@@ -5,45 +5,20 @@ import re
 from generate import generate, generate2
 import time
 
-## depreciated 
-#def parse(raw_data):
-#    b = []
-#    a = []
-#    re_split = re.split
-#    for row in raw_data:
-#        a_row = re_split(r"\s*;\s*", row)
-#        b.append(a_row.pop(len(a_row)-1))
-#        a.append(" ".join(a_row))
-#    
-#    a = ";".join(a)
-#    b = ";".join(b)
-#    return a, b
-#
-## depreciated
-#def create(a, b):
-#    # join the list with ";" (that's what numpy looking for)
-#    try:
-#        a = np.matrix(a)
-#        # Check if matrix is N by N+1
-#        if a.shape[0] != a.shape[1]:
-#            print "Matrix shape error"
-#            sys.exit(1)
-#        
-#        b = np.matrix(b)
-#        return a, b
-#    except SyntaxError:
-#        print "your matrix doesn't look right!"
-
 def parse_array(raw_data):
     """87.6549491771;59.8682503025;52.3886621673;44.5125050557;67.4679478072\n"""
-    start = time.time()
+    # TO-DO, handle trailing delimiter
     b = []
     a = []
+    last = None
     first_row = True
     length = 0
     re_split = re.split
     for row in raw_data:
         a_row = re_split(r"\s*;\s*", row)
+        last = a_row[len(a_row) - 1]
+        if not last: a_row.remove(last)
+        print a_row
         # a_row -> ["1","2","3","4"]
         b.append(a_row.pop(len(a_row)-1))
         a.append(a_row)
@@ -58,19 +33,15 @@ def parse_array(raw_data):
         print "Matrix size error"
         sys.exit(1)
 
-    print "parse_array(): ", str(time.time() - start)
     return a, b
     # two dimentinal python list a, b
 
 def create_array(a, b):
-    start = time.time()
     array_a = np.array(a)
     array_b = np.array(b)
-    print "create_array(): ", str(time.time() - start)
     return array_a, array_b 
 
 def solve(a, b, p = False):
-    start = time.time()
     try:
         x = np.linalg.solve(a, b)
     except ValueError:
@@ -83,7 +54,6 @@ def solve(a, b, p = False):
     if p:
         for i in range(x.size):
             print '%6.3f' % x.item(i)
-    print "solve(): ", str(time.time() - start)
 
 def test():
     global test_raw_data
@@ -92,34 +62,67 @@ def test():
     solve(A, b)
 
 def main():
-    global test_raw_data
-    TIME_TO_TEST = 10
+    global test_raw_data, raw_A, raw_b
+    N = 10
 
     if len(sys.argv) > 1:
-        if sys.argv[1] == "test":
+        if sys.argv[1] == "cProfile":
             if len(sys.argv) > 2:
-                TIME_TO_TEST = int(sys.argv[2])
-            from timeit import Timer
+                N = int(sys.argv[2])
             import cProfile
-            for i in xrange(1, TIME_TO_TEST):
+
+            # First Generate the data 
+            test_raw_data = generate2(N) # replace this with the one from professor
+
+            # Then test 
+
+            # 1. get profile
+            cProfile.runctx("parse_array(test_raw_data)", globals(), locals())
+
+            # 2. the actual run
+            raw_A, raw_b = parse_array(test_raw_data)
+
+            # 1. get profile
+            cProfile.runctx("solve(raw_A, raw_b)", globals(), locals())
+            # 2. the actual run
+            solve(raw_A, raw_b)
+        # end of cProfile
+
+        if sys.argv[1] == "timeit":
+            from timeit import Timer
+
+            if len(sys.argv) > 2: N = int(sys.argv[2])
+
+            # the size of marix will increase from 1 to N
+            for i in xrange(1, N):
                 # First Generate the data 
-                test_raw_data = generate2(i)
+                test_raw_data = generate2(i) # replace this with the one from professor
+
                 # Then test 
-                cProfile.runctx("parse_array(test_raw_data)", globals(), locals())
+
+                # 1. get profile
+                parse_time = Timer("parse_array(test_raw_data)", 
+                      "from __main__ import parse_array, test_raw_data").timeit(1)
+
+                # 2. the actual run
                 raw_A, raw_b = parse_array(test_raw_data)
 
-                cProfile.runctx("solve(raw_A, raw_b)", globals(), locals())
+                # 1. get profile
+                solve_time = Timer("solve(raw_A, raw_b)", 
+                      "from __main__ import solve, raw_A, raw_b").timeit(1)
+                # 2. the actual run
                 solve(raw_A, raw_b)
-                
-                
-                print "{0} {1}".format(str(i).ljust(8), str(t.timeit(1)).ljust(8))
+
+                print "%d    %f    %f" % (i, parse_time, solve_time)
+            # end of Timeit
         else:
-            print "option error"
-        # Testing block ends
-    else:
+            print "hrb-project0.py [cProfile N | timeit N]"
+    # Testing block ends
+
+    # non-testing mode
+    else: 
         raw_data = sys.stdin.readlines()
         raw_A, raw_b = parse_array(raw_data)
-
         solve(raw_A, raw_b, True)
 
 if __name__ == "__main__":
