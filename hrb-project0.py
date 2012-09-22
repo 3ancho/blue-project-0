a7c7c0230a25d241ff8d6caf7d71851b254de25e
Ruoran Wang <ruoranwang@gmail.com>
2012-09-21 21:23:56 -0400

    some tiny changes

--- hrb-project0.py
+++ hrb-project0.py
@@ -2,8 +2,6 @@
 import numpy as np
 import sys
 import re
-from generate import generate, generate2
-import time
 
 def parse_array(raw_data):
     """87.6549491771;59.8682503025;52.3886621673;44.5125050557;67.4679478072\n"""
@@ -18,7 +16,6 @@ def parse_array(raw_data):
         a_row = re_split(r"\s*;\s*", row)
         last = a_row[len(a_row) - 1]
         if not last: a_row.remove(last)
-        print a_row
         # a_row -> ["1","2","3","4"]
         b.append(a_row.pop(len(a_row)-1))
         a.append(a_row)
@@ -36,11 +33,6 @@ def parse_array(raw_data):
     return a, b
     # two dimentinal python list a, b
 
-def create_array(a, b):
-    array_a = np.array(a)
-    array_b = np.array(b)
-    return array_a, array_b 
-
 def solve(a, b, p = False):
     try:
         x = np.linalg.solve(a, b)
@@ -55,12 +47,6 @@ def solve(a, b, p = False):
         for i in range(x.size):
             print '%6.3f' % x.item(i)
 
-def test():
-    global test_raw_data
-    raw_A, raw_b = parse_array(test_raw_data)
-    A, b = create_array(raw_A, raw_b)
-    solve(A, b)
-
 def main():
     global test_raw_data, raw_A, raw_b
     N = 10
@@ -70,6 +56,7 @@ def main():
             if len(sys.argv) > 2:
                 N = int(sys.argv[2])
             import cProfile
+            from generate import generate2
 
             # First Generate the data 
             test_raw_data = generate2(N) # replace this with the one from professor
@@ -96,6 +83,7 @@ def main():
             # the size of marix will increase from 1 to N
             for i in xrange(1, N):
                 # First Generate the data 
+                from generate import generate2
                 test_raw_data = generate2(i) # replace this with the one from professor
 
                 # Then test 
@@ -113,7 +101,7 @@ def main():
                 # 2. the actual run
                 solve(raw_A, raw_b)
 
-                print "%d    %f    %f" % (i, parse_time, solve_time)
+                print "%3d, %8f, %8f" % (i, parse_time, solve_time)
             # end of Timeit
         else:
             print "hrb-project0.py [cProfile N | timeit N]"
